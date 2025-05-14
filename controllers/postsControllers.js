aedb84b79b4cce65f44363488fda8c7c35581218
EneaSema <sema.enea1993@gmail.com>
2025-05-14 16:27:27 +0200

    problems

--- controllers/postsControllers.js
+++ controllers/postsControllers.js
@@ -1,4 +1,86 @@
-const express = require("express");
-let postsController = express();
+const { posts } = require("../data/posts.js");
+const post = require("../routers/postsRouter.js");
 
-module.exports = { postsController };
+const index = (req, res) => {
+  res.json({
+    status: 200,
+    description: "Post totali",
+    data: posts,
+  });
+};
+
+const show = (req, res) => {
+  const id = parseInt(req.params.id);
+  const post = posts.find((currentPost) => currentPost.id === id);
+
+  if (!post) {
+    return res.json({
+      status: 404,
+      error: "Not Found",
+      messagge: "Post non trovato",
+    });
+  }
+  res.json({
+    status: 200,
+    description: "Ecco il post scelto:" + id,
+    data: post,
+  });
+};
+
+const store = (req, res) => {
+  res.send("Ecco il nuovo post creato");
+
+  if (!post) {
+    return res.json({
+      status: 404,
+      error: "Not Found",
+      messagge: "Post non trovato",
+    });
+  }
+};
+
+const update = (req, res) => {
+  const id = parseInt(req.params.id);
+  const post = posts.find((currentPost) => currentPost.id === id);
+  res.send("Ecco il post scelto per la modifica totale:" + id);
+
+  if (!post) {
+    return res.json({
+      status: 404,
+      error: "Not Found",
+      messagge: "Post non trovato",
+    });
+  }
+};
+
+const modify = (req, res) => {
+  const id = parseInt(req.params.id);
+  const post = posts.find((currentPost) => currentPost.id === id);
+  res.send("Ecco il post scelto per la modifica parziale:" + id);
+
+  if (!post) {
+    return res.json({
+      status: 404,
+      error: "Not Found",
+      messagge: "Post non trovato",
+    });
+  }
+};
+
+const destroy = (req, res) => {
+  const id = parseInt(req.params.id);
+  const post = posts.find((currentPost) => currentPost.id === id);
+  res.send("Ecco il post scelto per eliminazione:" + id);
+  if (!post) {
+    return res.json({
+      status: 404,
+      error: "Not Found",
+      messagge: "Post non trovato",
+    });
+  }
+  post.splice(posts.indexOf(post), 1);
+  console.log(posts);
+  res.sendStatus(204);
+};
+
+module.exports = { index, show, store, update, modify, destroy };
--- routers/postsRouter.js
+++ routers/postsRouter.js
@@ -1,99 +1,30 @@
 const express = require("express");
 const post = express.Router();
-const { posts } = require("../data/posts.js");
+
+const postController = require("../controllers/postsControllers.js");
 
 // Risposta alla richiesta di lettura di tutti i post
 
-post.get("/", (req, res) => {
-  res.json({
-    status: 200,
-    description: "Post totali",
-    data: posts,
-  });
-});
+post.get("/", postController.index);
 
 // Risposta alla richiesta di lettura di un post specifico
 
-post.get("/:id", (req, res) => {
-  const id = parseInt(req.params.id);
-  const post = posts.find((currentPost) => currentPost.id === id);
-
-  if (!currentPost) {
-    return res.json({
-      status: 404,
-      error: "Not Found",
-      messagge: "Post non trovato",
-    });
-  }
-  res.json({
-    status: 200,
-    description: "Ecco il post scelto:" + id,
-    data: post[id],
-  });
-});
+post.get("/:id", postController.show);
 
 // Risposta alla richiesta di creazione di un post
 
-post.post("/", (req, res) => {
-  res.send("Ecco il nuovo post creato");
-
-  if (!currentPost) {
-    return res.json({
-      status: 404,
-      error: "Not Found",
-      messagge: "Post non trovato",
-    });
-  }
-});
+post.post("/", postController.store);
 
 // Risposta alla richiesta di modifica totale di un post specifico
 
-post.put("/:id", (req, res) => {
-  const id = parseInt(req.params.id);
-  const post = posts.find((currentPost) => currentPost.id === id);
-  res.send("Ecco il post scelto per la modifica totale:" + id);
-
-  if (!currentPost) {
-    return res.json({
-      status: 404,
-      error: "Not Found",
-      messagge: "Post non trovato",
-    });
-  }
-});
+post.put("/:id", postController.update);
 
 // Risposta alla richiesta di modifica parziale di un post specifico
 
-post.patch("/:id", (req, res) => {
-  const id = parseInt(req.params.id);
-  const post = posts.find((currentPost) => currentPost.id === id);
-  res.send("Ecco il post scelto per la modifica parziale:" + id);
-
-  if (!currentPost) {
-    return res.json({
-      status: 404,
-      error: "Not Found",
-      messagge: "Post non trovato",
-    });
-  }
-});
+post.patch("/:id", postController.modify);
 
 // Risposta alla richiesta di elimazione di un post specifico
 
-post.delete("/:id", (req, res) => {
-  const id = parseInt(req.params.id);
-  const post = posts.find((currentPost) => currentPost.id === id);
-  res.send("Ecco il post scelto per eliminazione:" + id);
-  if (!currentPost) {
-    return res.json({
-      status: 404,
-      error: "Not Found",
-      messagge: "Post non trovato",
-    });
-  }
-  post.splice(posts.indexOf(post), 1);
-  console.log(posts);
-  res.sendStatus(204);
-});
+post.delete("/:id", postController.destroy);
 
 module.exports = post;
